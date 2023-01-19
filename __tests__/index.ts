6d364328445acd58a6ea9cb7e4383532b7939f6e
leowang <rfv7855659@gmail.com>
2023-01-19 15:58:21 +0800

    ✨重構 promise & test

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -1,111 +1,90 @@
 
 import Promise from '../src/promise'
 describe('Promise', () => {
-    it('Promise 是一個class', () => {
-        expect(Promise).toBe(Function)
-        expect(Promise.prototype).toBe(Object)
+    it('Promise 是一個 class', () => {
+        expect(new Promise(() => { })).toBeInstanceOf(Promise)
+        expect(Promise).toBeInstanceOf(Function)
+        expect(Promise.prototype).toBeInstanceOf(Object)
     })
-    // describe('傳入的 fn', () => {
-    //     it('new Promise 必須接受一個函數', () => {
-    //         assert.throw(() => {
-    //             // @ts-ignore
-    //             new Promise(1)
-    //         })
-    //     })
+    describe('傳入的 fn', () => {
+        it('new Promise 必須接受一個函數', () => {
+            // @ts-ignore
+            expect(() => { new Promise(1) }).toThrow()
+        })
 
-    //     it('new Promise(fn) 中的 fn 會立即執行。', () => {
-    //         let fn = sinon.fake()
-    //         new Promise(fn)
-    //         assert.isTrue(fn.called)
-    //     })
+        it('new Promise(fn) 中的 fn 會立即執行。', () => {
+            let fn = jest.fn()
+            new Promise(fn)
+            expect(fn).toBeCalled()
+        })
 
-    //     it('new Promise(fn) 中的 fn 執行時接收 resolve 與 reject 兩個函數', (done) => {
-    //         let fn = sinon.fake()
-    //         new Promise((resolve, reject) => {
-    //             assert.isFunction(resolve)
-    //             assert.isFunction(reject)
-    //             done()
-    //         })
-    //         assert.isTrue(fn.called)
-    //     })
+        it('new Promise(fn) 中的 fn 執行時接收 resolve 與 reject 兩個函數', (done) => {
+            let fn = jest.fn((resolve: Function, reject: Function) => {
+                expect(resolve).toBeInstanceOf(Function)
+                expect(reject).toBeInstanceOf(Function)
+                done()
+            })
+            new Promise(fn)
+            expect(fn).toBeCalled()
+        })
 
-    // })
-    // describe('狀態', () => {
-    //     it('new Promise 呼叫時 status 狀態為 pending', () => {
-    //         let fn = sinon.fake()
-    //         let promise = new Promise(fn)
-    //         assert(promise.status === 'pending')
-    //     })
-    //     it('new Promise 使用 resolve 後狀態改為 fulfilled', (done) => {
-    //         let promise = new Promise((resolve) => {
-    //             resolve()
-    //             setTimeout(() => {
-    //                 done()
-    //             }, 0)
-    //         })
-    //         assert(promise.status === 'fulfilled')
-    //     })
-    //     it('new Promise 使用 reject 後狀態改為 rejected', (done) => {
-    //         let promise = new Promise((resolve, reject) => {
-    //             reject()
-    //             setTimeout(() => {
-    //                 done()
-    //             }, 0)
-    //         })
-    //         assert(promise.status === 'rejected')
-    //     })
+    })
+    describe('狀態', () => {
+        it('new Promise 呼叫時 status 狀態為 pending', () => {
+            let fn = jest.fn()
+            let promise = new Promise(fn)
+            expect(promise.status).toBe('pending')
+        })
+        it('new Promise 使用 resolve 後狀態改為 fulfilled', (done) => {
+            let promise = new Promise((resolve: Function) => {
+                resolve()
+                done()
+            })
+            expect(promise.status).toBe('fulfilled')
+        })
+        it('new Promise 使用 reject 後狀態改為 rejected', (done) => {
+            let promise = new Promise((resolve: Function, reject: Function) => {
+                reject()
+                done()
+            })
+            expect(promise.status).toBe('rejected')
+        })
 
-    //     it('狀態值從 pending 轉為 fulfilled 後，不會再被修改', (done) => {
-    //         let promise = new Promise((resolve, reject) => {
-    //             resolve()
-    //             reject()
-    //             setTimeout(() => {
-    //                 done()
-    //             }, 0)
-    //         })
-    //         assert(promise.status === 'fulfilled')
-    //     })
+        it('狀態值從 pending 轉為 fulfilled 後，不會再被修改', (done) => {
+            let promise = new Promise((resolve: Function, reject: Function) => {
+                resolve()
+                reject()
+                done()
+            })
+            expect(promise.status).toBe('fulfilled')
+        })
 
-    //     it('狀態值從 pending 轉為 rejected 後，不會再被修改', (done) => {
-    //         let promise = new Promise((resolve, reject) => {
-    //             reject()
-    //             resolve()
-    //             setTimeout(() => {
-    //                 done()
-    //             }, 0)
-    //         })
-    //         assert(promise.status === 'rejected')
-    //     })
+        it('狀態值從 pending 轉為 rejected 後，不會再被修改', (done) => {
+            let promise = new Promise((resolve: Function, reject: Function) => {
+                reject()
+                resolve()
+                done()
+            })
+            expect(promise.status).toBe('rejected')
+        })
 
-    // })
+    })
 
-    // describe('resolve', () => {
-    //     it('能夠傳字串，並且添加到 result 中', (done) => {
-    //         let promise = new Promise((resolve) => {
-    //             resolve('你好')
-    //             setTimeout(() => {
-    //                 done()
-    //             }, 0)
-    //         })
-    //         assert(promise.result === '你好')
-    //     })
+    describe('resolve', () => {
+        it('能夠傳字串，並且添加到 result 中', (done) => {
+            let promise = new Promise((resolve: Function) => {
+                resolve('你好')
+                done()
+            })
+            expect(promise.result).toBe('你好')
+        })
 
-    //     it('能夠傳數字，並且添加到 result 中', (done) => {
-    //         let promise = new Promise((resolve) => {
-    //             resolve(1)
-    //             setTimeout(() => {
-    //                 done()
-    //             }, 0)
-    //         })
-    //         assert(promise.result === 1)
-    //     })
-
-    //     it('能夠傳字串，並且添加到 result 中', (done) => {
-    //         let promise = new Promise((resolve) => {
-    //             resolve('你好')
-    //             done()
-    //         })
-    //         assert(promise.result === '你好')
-    //     })
-    // })
+        it('能夠傳數字，並且添加到 result 中', (done) => {
+            let promise = new Promise((resolve: Function) => {
+                resolve(1)
+                done()
+            })
+            expect(promise.result).toBe(1)
+        })
+    })
 })
--- src/promise.ts
+++ src/promise.ts
@@ -1,45 +1,30 @@
-class PromisePolyfill {
-    status = "pending"
-    callbacks = []
-    resolve(result) {
-        setTimeout(() => {
-            if(this.status !== 'pending') return 
-            this.status = "fulfilled"
-            this.callbacks.forEach(handle=>{
-                if (typeof handle[0] === 'function') {
-                    handle[0].call(undefined,result)
-                }
-            })
-        }, 0)
-    }
-    reject(reason) {
-        setTimeout(() => {
-            if(this.status !== 'pending') return 
-            this.status = "rejected"
-            this.callbacks.forEach(handle=>{
-                if (typeof handle[1] === 'function') {
-                    handle[1].call(undefined,reason)
-                }
-            })
-        }, 0)
-    }
-    constructor(fn) {
-        if (typeof fn !== 'function') {
-            throw new Error('只接收函數')
-        }
+export default class Promise {
+    static PENDING = 'pending'
+    static FULFILLED = 'fulfilled'
+    static REJECTED = 'rejected'
+    status: string
+    result: null
+    constructor(fn: Function) {
+        this.status = Promise.PENDING
+        this.result = null
         fn(this.resolve.bind(this), this.reject.bind(this))
     }
-    then(succeed?, fail?) {
-        const handle = []
-        if (typeof succeed === 'function') {
-            handle[0] = succeed
-        }
-        if (typeof fail === 'function') {
-            handle[1] = fail
-        }
-        this.callbacks.push(handle)
-        return new PromisePolyfill(()=>{})
-    }
-}
 
-export default PromisePolyfill
+    resolve(result: any) {
+        if (this.status === Promise.PENDING) {
+            this.status = Promise.FULFILLED
+            this.result = result
+        }
+    }
+
+    reject(reason: any) {
+        if (this.status === Promise.PENDING) {
+            this.status = Promise.REJECTED
+            this.result = reason
+        }
+    }
+
+    then() {
+
+    }
+} 
